18a68fc83709058c4650f91bd4aee0696eeebb17
Jenny CHE <7469@PST6915.sciences-po.fr>
2017-11-17 15:49:33 +0100

    gourmandes

---
+++ exercice.js
@@ -0,0 +1,20 @@
+var gourmandes = [   
+  {
+    nom: "Che",
+    prenom: "Jenny",
+    age: 25,
+    afaim: true,
+    chocolat: 'noir',
+    vegan: false,
+    cuisinier: true
+  },
+  {
+    nom: "Courtois",
+    prenom: "Maia",
+    age: 21,
+    afaim: true,
+    chocolat: 'noir',
+    vegan: false,
+    cuisinier: true
+  }
+]
